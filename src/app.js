26b6e71d88632643e31412c33f4e39b156e9ae8e
LuisaSuarez09 <fernandasuarez2020@gmail.com>
2024-05-29 18:58:15 -0500

    Configuracion de plantillas

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
-import express from "express"
+import express from "express";
+import morgan from 'morgan';
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {engine} from 'express-handlebars'
 
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.set("port", process.env.PORT || 4000)
+app.set("port", process.env.PORT || 3000)
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.set('views', join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialDir: join(app.get('views'), 'partial'),
+    extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.json({ "message": "Welcom"})
-})
+    res.render('index')
+});
+
+app.use(express.static(join(__dirname, 'public')));
 
 export default app;
